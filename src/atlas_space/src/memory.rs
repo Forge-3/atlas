f4a96d49ed8a52c6d7e164cd044ab07dd5dfac86
odzioo123 <1konrad.sadowski@gmail.com>
2025-06-30 00:12:35 +0200

    Backend almost done (no frontend adaptation)
    Added start_time and end_time fields to Task and CreateTaskArgs
    
    Added type ClosedTask that should have all Task fields, but doesn't store all submissions, just accepted. ClosedTask should be created from Task.
    
    Added method to close task in memory.rs.
    
    Changed CLOSED_TASKS_MAP, to use ClosedTask instead of Task.
    
    Added metods to update.rs to force close task and to withdraw remaining funds from Task as creator.

--- src/atlas_space/src/memory.rs
+++ src/atlas_space/src/memory.rs
@@ -190,6 +190,27 @@ pub fn get_closed_tasks_len() -> u64 {
     CLOSED_TASKS_MAP.with_borrow(|tasks| tasks.len())
 }
 
+// Other methods
+
+pub fn close_task(task_id: TaskId) -> Result<(), Error> {
+    let task = remove_open_task(&task_id)?;
+    let closed_task: ClosedTask = task.into();
+    
+    insert_closed_task(task_id, closed_task)?;
+    Ok(())
+}
+
+pub fn close_task_if_expired(task_id: TaskId) -> Result<bool, Error> {
+    let task = get_open_task(&task_id).ok_or(Error::TaskNotFound(task_id))?;
+
+    if task.is_expired() {
+        close_task(task_id)?;
+        Ok(true)
+    } else {
+        Ok(false)
+    }
+}
+
 pub fn delete_closed_task(task_id: &TaskId) -> Option<ClosedTask> {
     CLOSED_TASKS_MAP.with_borrow_mut(|tasks| tasks.remove(task_id))
 }
